9ec7c2a4613000eaafeb2a0641d88075d76a429e
yzhu <yzhu@ucla.edu>
2020-07-22 21:12:58 -0700

    feat: add function to calculate equal opportunity difference

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import numpy as np
-from scipy.special import gamma,psi
+from scipy.special import gamma, psi
 from scipy import ndimage
 from scipy.linalg import det
 from numpy import pi
 
 from sklearn.neighbors import NearestNeighbors
 
-__all__=['entropy', 'mutual_information', 'entropy_gaussian', 'mutual_information_2d']
+__all__ = [
+    'entropy', 'mutual_information', 'entropy_gaussian',
+    'mutual_information_2d'
+]
 
 EPS = np.finfo(float).eps
 
@@ -20,19 +23,19 @@ def nearest_distances(X, k=1):
     '''
     knn = NearestNeighbors(n_neighbors=k + 1)
     knn.fit(X)
-    d, _ = knn.kneighbors(X) # the first nearest neighbor is itself
-    return d[:, -1] # returns the distance to the kth nearest neighbor
+    d, _ = knn.kneighbors(X)  # the first nearest neighbor is itself
+    return d[:, -1]  # returns the distance to the kth nearest neighbor
 
 
 def entropy_gaussian(C):
     '''
     Entropy of a gaussian variable with covariance matrix C
     '''
-    if np.isscalar(C): # C is the variance
-        return .5*(1 + np.log(2*pi)) + .5*np.log(C)
+    if np.isscalar(C):  # C is the variance
+        return .5 * (1 + np.log(2 * pi)) + .5 * np.log(C)
     else:
-        n = C.shape[0] # dimension
-        return .5*n*(1 + np.log(2*pi)) + .5*np.log(abs(det(C)))
+        n = C.shape[0]  # dimension
+        return .5 * n * (1 + np.log(2 * pi)) + .5 * np.log(abs(det(C)))
 
 
 def entropy(X, k=1):
@@ -55,17 +58,17 @@ def entropy(X, k=1):
     '''
 
     # Distance to kth nearest neighbor
-    r = nearest_distances(X, k) # squared distances
+    r = nearest_distances(X, k)  # squared distances
     n, d = X.shape
-    volume_unit_ball = (pi**(.5*d)) / gamma(.5*d + 1)
+    volume_unit_ball = (pi**(.5 * d)) / gamma(.5 * d + 1)
     '''
     F. Perez-Cruz, (2008). Estimation of Information Theoretic Measures
     for Continuous Random Variables. Advances in Neural Information
     Processing Systems 21 (NIPS). Vancouver (Canada), December.
     return d*mean(log(r))+log(volume_unit_ball)+log(n-1)-log(k)
     '''
-    return (d*np.mean(np.log(r + np.finfo(X.dtype).eps))
-            + np.log(volume_unit_ball) + psi(n) - psi(k))
+    return (d * np.mean(np.log(r + np.finfo(X.dtype).eps)) +
+            np.log(volume_unit_ball) + psi(n) - psi(k))
 
 
 def mutual_information(variables, k=1):
@@ -81,10 +84,9 @@ def mutual_information(variables, k=1):
     '''
     if len(variables) < 2:
         raise AttributeError(
-                "Mutual information must involve at least 2 variables")
+            "Mutual information must involve at least 2 variables")
     all_vars = np.hstack(variables)
-    return (sum([entropy(X, k=k) for X in variables])
-            - entropy(all_vars, k=k))
+    return (sum([entropy(X, k=k) for X in variables]) - entropy(all_vars, k=k))
 
 
 def mutual_information_2d(x, y, sigma=1, normalized=False):
@@ -109,8 +111,7 @@ def mutual_information_2d(x, y, sigma=1, normalized=False):
     jh = np.histogram2d(x, y, bins=bins)[0]
 
     # smooth the jh with a gaussian filter of given sigma
-    ndimage.gaussian_filter(jh, sigma=sigma, mode='constant',
-                                 output=jh)
+    ndimage.gaussian_filter(jh, sigma=sigma, mode='constant', output=jh)
 
     # compute marginal histograms
     jh = jh + EPS
@@ -124,10 +125,51 @@ def mutual_information_2d(x, y, sigma=1, normalized=False):
     # "A normalized entropy measure of 3-D medical image alignment".
     # in Proc. Medical Imaging 1998, vol. 3338, San Diego, CA, pp. 132-143.
     if normalized:
-        mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2)))
-                / np.sum(jh * np.log(jh))) - 1
+        mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2))) / np.sum(
+            jh * np.log(jh))) - 1
     else:
-        mi = ( np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1))
-               - np.sum(s2 * np.log(s2)))
+        mi = (np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1)) -
+              np.sum(s2 * np.log(s2)))
 
     return mi
+
+
+def equal_opportunity_difference(Y_true, Y_pred, sensitive_features):
+    """
+    Compute equal opportunity difference for a binary dataset.
+    The difference is calculated by the abosolute value of 
+    P(y_pred = 1| y_true = 1, sensitive_feature=1) 
+    − P(y_pred = 1 | y_true = 1, sensitive_feature=0′)
+    Parameters
+    ----------
+    y_true: 1D array
+        ground truth labels
+    y_pred : 1D array
+        predicted labels
+    sensitive features: 1D array
+        sensitive features
+    Returns
+    -------
+    equal_opportunity_difference: float
+        the difference of equal opportunity
+    """
+    count1, count2, count3, count4 = 0, 0, 0, 0
+    # Count1: count the case that y_true=1, y_pred=1, A=1
+    # Count2: count the case that y_true=1, A=1
+    # Count3: count the case that y_true=1, y_pred=1, A=0
+    # Count4: count the case that y_true=1, A=0
+    assert len(Y_true) == len(Y_pred) == len(sensitive_features)
+    for i in range(len(Y_true)):
+        if Y_true[i] != 1:
+            continue
+        if sensitive_features[i] == 1:
+            count2 += 1
+        elif sensitive_features[i] == 0:
+            count4 += 1
+        if Y_pred[i] != 1:
+            continue
+        if sensitive_features[i] == 1:
+            count1 += 1
+        elif sensitive_features[i] == 0:
+            count3 += 1
+    return abs(1. * count1 / count2 - 1. * count3 / count4)
